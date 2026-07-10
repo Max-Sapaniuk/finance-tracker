09228d78ab4d13fa656309e4719ab2848345035d
Max-Sapaniuk <maksym.sapaniuk@gmail.com>
2026-07-10 18:28:05 +0300

    add shadcn, update design system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Space_Grotesk } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/shared/lib/utils';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +31,15 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        'h-full',
+        'antialiased',
+        geistSans.variable,
+        geistMono.variable,
+        'font-sans',
+        spaceGrotesk.variable,
+        'dark',
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
